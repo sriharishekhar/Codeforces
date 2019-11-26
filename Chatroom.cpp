637047958053642cf680fe3e0ac5fdbc95a08628
Srihari Shekhar <prime.sriharishekhar@gmail.com>
2019-11-27 03:30:03 +0530

    Solution

---
+++ Chatroom.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+string s1, s2 = "hello";
+int k = 0, pass = 0;
+int main() {
+    cin >> s1;
+    for (int i = 0; i < s1.size(); i++) {
+        if (s1[i] == s2[k]) {
+            k++;
+            pass++;
+            
+            if (pass == 5) {
+                break;
+            }
+        }
+    }
+    if (pass == 5) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
