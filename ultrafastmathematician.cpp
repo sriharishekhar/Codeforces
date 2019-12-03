62a68bddd909f73ab641fd4c10694fd4f979f5d3
Srihari Shekhar <prime.sriharishekhar@gmail.com>
2019-12-03 13:11:14 -0800

    https://codeforces.com/problemset/problem/61/A

---
+++ ultrafastmathematician.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1; 
+  cin >> s2;
+  for (int i = 0; i < s1.size(); i++) {  
+    if(s1[i] == s2[i]) s1[i] = '0';
+    else s1[i] = '1';
+  }
+  for (int i = 0; i < s1.size(); i++) {
+    cout << s1[i];
+  }
+  return 0;
+}
