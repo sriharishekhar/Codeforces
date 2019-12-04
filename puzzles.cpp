4158c7dcff3d10adf786cf2ed5249d522fe81588
Srihari Shekhar <prime.sriharishekhar@gmail.com>
2019-12-03 17:29:47 -0800

    https://codeforces.com/contest/337/problem/A

--- puzzles.cpp
+++ puzzles.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <algorithm>
 
-
 int main() { 
   int n, m, a;
   std::cin >> n >> m;
@@ -13,9 +12,7 @@ int main() {
   }
   sort(v.begin(), v.end());
   std::vector<int> puzzle;
-  for (int i = 0; i+n <= m; i++) {
-    puzzle.push_back(v[i+n-1] - v[i]);
-  }
+  for (int i = 0; i+n <= m; i++) puzzle.push_back(v[i+n-1] - v[i]);
   sort(puzzle.begin(), puzzle.end());
   std::cout << puzzle[0];
   return 0;
